b2ef629a5c9fecc7a08d02db4783260a4f6aff49
tharun prakash <tharunprakash999@gmail.com>
2025-08-11 23:56:35 +0530

    First commit

---
+++ veritas-ui FrontEnd/veritas-ui/src/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import UploadSection from "./UploadSection";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <UploadSection />
+    </div>
+  );
+}
+
+export default App;
---
+++ veritas-ui-backend/src/main/java/com/example/Veritas/AI/VeritasAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Veritas.AI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeritasAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeritasAiApplication.class, args);
+	}
+
+}
